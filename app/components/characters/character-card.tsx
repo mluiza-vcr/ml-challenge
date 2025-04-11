b0db4a17023a55a24696b93725972b8068d892f3
Maria Luiza Castelar <mluiza.vcr@gmail.com>
2025-04-11 14:22:45 -0300

    feat: add getGenderLabel translation

--- app/components/characters/character-card.tsx
+++ app/components/characters/character-card.tsx
@@ -11,15 +11,28 @@ export function CharacterCard({
   gender: string;
   planetName: string;
 }) {
+  function getGenderLabel(gender: string): string {
+    switch (gender) {
+      case "female":
+        return "Gênero feminino";
+      case "male":
+        return "Gênero masculino";
+      case "hermaphrodite":
+        return "Hermafrodita";
+      default:
+        return "Gênero desconhecido";
+    }
+  }
+
   return (
     <div className="w-full lg:min-w-80 lg:w-1/6 cursor-pointer">
       <Link to="/home">
-        <BackgroundGradient className="rounded-[22px] p-4 sm:p-8 bg-white dark:bg-zinc-900 lg:h-64">
-          <p className="sm:text-xl lg:text-2xl text-black mt-4 mb-2 dark:text-neutral-200">
+        <BackgroundGradient className="rounded-[22px] p-4 sm:p-8 bg-white dark:bg-zinc-900 lg:h-64 text-center">
+          <p className="sm:text-xl font-bold lg:text-2xl text-black mt-4 mb-2 dark:text-neutral-200">
             {name}
           </p>
-          <p className="lg:mt-10 text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
-            {gender}
+          <p className="lg:mt-10 sm:text-lg text-black mt-4 mb-2 dark:text-neutral-200">
+            {getGenderLabel(gender)}
           </p>
           <p className="text-sm lg:text-base text-neutral-600 dark:text-neutral-400">
             Planeta {planetName === "unknown" ? "Desconhecido" : planetName}
