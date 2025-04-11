5d4aeb10c0c135eaea4e77676786251ed2af2478
Maria Luiza Castelar <mluiza.vcr@gmail.com>
2025-04-11 14:23:07 -0300

    feat: update home pagecwith text effect

--- app/components/home/home-page.tsx
+++ app/components/home/home-page.tsx
@@ -1,13 +1,12 @@
+import { TextGenerateEffect } from "../ui/text-generate-effect";
+
 export default function HomePage() {
+  const starWarsIntro =
+    "It is a period of civil war. Rebel spaceships, striking from a hidden base, have won their first victory against the evil Galactic Empire. During the battle, Rebel spies managed to steal secret plans to the Empire’s ultimate weapon, the DEATH STAR, an armored space station with enough power to destroy an entire planet. Pursued by the Empire’s sinister agents, Princess Leia races home aboard her starship, custodian of the stolen plans that can save her people and restore freedom to the galaxy.";
+
   return (
-    <div>
-      It is a period of civil war. Rebel spaceships, striking from a hidden
-      base, have won their first victory against the evil Galactic Empire.
-      During the battle, Rebel spies managed to steal secret plans to the
-      Empire’s ultimate weapon, the DEATH STAR, an armored space station with
-      enough power to destroy an entire planet. Pursued by the Empire’s sinister
-      agents, Princess Leia races home aboard her starship, custodian of the
-      stolen plans that can save her people and restore freedom to the galaxy
+    <div className="px-6 text-justify">
+      <TextGenerateEffect words={starWarsIntro} />{" "}
     </div>
   );
 }
