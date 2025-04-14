260663445a4d665b7f2fe53695555489ccc8d11f
Maria Luiza Castelar <mluiza.vcr@gmail.com>
2025-04-14 01:59:15 -0300

    feat: add favorite logic

--- app/components/characters/character-card.tsx
+++ app/components/characters/character-card.tsx
@@ -1,7 +1,10 @@
 "use client";
+import type React from "react";
 import { getGenderLabel } from "~/utils/getLabels";
 import { BackgroundGradient } from "../ui/background-gradient";
 import { Link } from "@remix-run/react";
+import { useEffect, useState } from "react";
+import { Star } from "lucide-react";
 
 export function CharacterCard({
   name,
@@ -19,10 +22,55 @@ export function CharacterCard({
     return match ? match[1] : null;
   }
 
+  const id = extractIdFromUrl(url);
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  useEffect(() => {
+    if (!id) return;
+    const stored = localStorage.getItem("favorites");
+    const favorites = stored ? JSON.parse(stored) : [];
+    setIsFavorite(favorites.includes(id));
+  }, [id]);
+
+  function toggleFavorite(e: React.MouseEvent) {
+    e.preventDefault(); // impedir navegação ao clicar na estrela
+    if (!id) return;
+
+    const stored = localStorage.getItem("favorites");
+    const favorites = stored ? JSON.parse(stored) : [];
+
+    // biome-ignore lint/suspicious/noImplicitAnyLet: <explanation>
+    let updatedFavorites;
+    if (favorites.includes(id)) {
+      updatedFavorites = favorites.filter((favId: string) => favId !== id);
+    } else {
+      updatedFavorites = [...favorites, id];
+    }
+
+    localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
+    setIsFavorite(updatedFavorites.includes(id));
+  }
+
   return (
     <div className="w-full lg:min-w-80 lg:w-1/6 cursor-pointer">
       <Link to={`/personagens/${extractIdFromUrl(url)}`}>
         <BackgroundGradient className="rounded-[22px] p-4 sm:p-8 bg-white dark:bg-zinc-900 lg:h-56 text-center">
+          <div
+            onClick={toggleFavorite}
+            className="absolute top-4 right-4 text-purple-600 hover:scale-110 transition-transform"
+            onKeyDown={(e) => {
+              if (e.key === "Enter" || e.key === " ") {
+                e.preventDefault();
+                toggleFavorite(e as unknown as React.MouseEvent);
+              }
+            }}
+          >
+            <Star
+              fill={isFavorite ? "rgb(147 51 234)" : "none"}
+              stroke="currentColor"
+              className="w-6 h-6"
+            />
+          </div>
           <p className="sm:text-xl font-bold lg:text-2xl text-black mt-4 mb-2 dark:text-neutral-200">
             {name}
           </p>
