9767143cc26016c93100d88cbdf0bade6360a7c5
Maria Luiza Castelar <mluiza.vcr@gmail.com>
2025-04-15 01:28:04 -0300

    feat: adjust title

--- app/components/layout/title.tsx
+++ app/components/layout/title.tsx
@@ -4,7 +4,7 @@ import { AudioPlayer } from "./audio-player";
 
 export default function Title({ children }: { children: ReactNode }) {
   return (
-    <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:pl-6 flex flex-col items-center lg:flex-row lg:justify-between">
+    <h1 className="scroll-m-20 text-xl lg:text-4xl font-bold tracking-tight lg:pl-6 flex flex-col items-center lg:flex-row lg:justify-between">
       <div>
         <span>{children}</span>
         <BackNavigationButton />
