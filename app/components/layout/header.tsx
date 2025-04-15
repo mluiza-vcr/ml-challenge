cea2e0dda47d21141f831c2db92e41f80bd62a72
Maria Luiza Castelar <mluiza.vcr@gmail.com>
2025-04-15 11:13:57 -0300

    feat: adjust audioplayer button

--- app/components/layout/header.tsx
+++ app/components/layout/header.tsx
@@ -3,6 +3,7 @@ import Logo from "/logo-star-wars.png";
 import ToggleTheme from "./toggle-theme";
 import NavBar from "./nav-bar";
 import { MobileNavBar } from "./mobile-nav-bar";
+import { AudioPlayer } from "./audio-player";
 
 export default function Header() {
   return (
@@ -10,9 +11,14 @@ export default function Header() {
       className="flex justify-between h-20 items-center px-4 border-b supports-backdrop-blur:bg-background/60 bg-background/60 backdrop-blur z-50"
       style={{ gridArea: "header" }}
     >
-      <Link to="/">
-        <img src={Logo} alt="logotipo" className="w-16" />
-      </Link>
+      <div className="flex items-center gap-2">
+        <Link to="/">
+          <img src={Logo} alt="logotipo" className="w-16" />
+        </Link>
+        <div className="">
+          <AudioPlayer />
+        </div>
+      </div>
 
       {/* Desktop Nav */}
       <div className="hidden lg:flex">
