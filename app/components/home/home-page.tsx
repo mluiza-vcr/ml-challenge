4dafe9262401f3c30306bf86f84ead0d3bfd2dae
Maria Luiza Castelar <mluiza.vcr@gmail.com>
2025-04-15 17:15:23 -0300

    feat: adjust mt on home page cards

--- app/components/home/home-page.tsx
+++ app/components/home/home-page.tsx
@@ -5,7 +5,7 @@ import { CardBody, CardContainer, CardItem } from "../ui/3d-card";
 export default function HomePage() {
   return (
     <>
-      <div className="flex flex-col lg:flex-row justify-center lg:gap-2 -mb-16 -mt-16">
+      <div className="flex flex-col lg:flex-row justify-center lg:gap-2 -mb-16 -mt-16 lg:mt-0">
         <CardContainer>
           <CardBody className="bg-gray-50 relative hover:shadow-2xl hover:shadow-emerald-400/[0.2] bg-transparent dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
             <CardItem
